8d998c9f3ac58faa85ad5c0bb777cccae3fb13e1
darksv <darek969-12@o2.pl>
2022-02-26 20:31:02 +0100

    even more

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(let_else)]
+
 use std::{fmt, fs};
 use std::fs::read;
 use std::io::{BufRead, BufReader};
@@ -75,7 +77,7 @@ struct Event<'s> {
 }
 
 #[derive(Debug)]
-struct LineMapping {
+struct EventMapping {
     marked: usize,
     layer: usize,
     start: usize,
@@ -89,9 +91,9 @@ struct LineMapping {
     text: usize,
 }
 
-impl Default for LineMapping {
+impl Default for EventMapping {
     fn default() -> Self {
-        LineMapping {
+        EventMapping {
             marked: usize::MAX,
             layer: usize::MAX,
             start: usize::MAX,
@@ -107,8 +109,8 @@ impl Default for LineMapping {
     }
 }
 
-fn make_mapping(config: &str) -> Option<LineMapping> {
-    let mut mapping = LineMapping::default();
+fn parse_events_mapping(config: &str) -> Option<EventMapping> {
+    let mut mapping = EventMapping::default();
     for (idx, field) in config.split(", ").enumerate() {
         match field {
             "Layer" => mapping.layer = idx,
@@ -129,10 +131,11 @@ fn make_mapping(config: &str) -> Option<LineMapping> {
     Some(mapping)
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Debug)]
 enum Section {
     Events,
     ScriptInto,
+    V4Styles,
 }
 
 fn main() {
@@ -141,11 +144,198 @@ fn main() {
     dbg!(a.elapsed());
 }
 
+struct ScriptInfo {
+    wrap_style: u32,
+    scaled_border_and_shadow: bool,
+    title: String,
+    play_res_x: u32,
+    play_res_y: u32,
+    script_type: String,
+    ycb_cr_matrix: String,
+    original_translation: String,
+    last_style_storage: String,
+    audio_file: String,
+    video_file: String,
+    video_ar_value: f32,
+    video_zoom_percent: f32,
+    active_line: u32,
+    video_position: u32,
+}
+
+
+struct StyleMapping {
+    name: usize,
+    font_name: usize,
+    font_size: usize,
+    primary_colour: usize,
+    secondary_colour: usize,
+    outline_colour: usize,
+    back_colour: usize,
+    bold: usize,
+    italic: usize,
+    underline: usize,
+    strike_out: usize,
+    scale_x: usize,
+    scale_y: usize,
+    spacing: usize,
+    angle: usize,
+    border_style: usize,
+    outline: usize,
+    shadow: usize,
+    alignment: usize,
+    margin_l: usize,
+    margin_r: usize,
+    margin_v: usize,
+    encoding: usize,
+}
+
+#[derive(Debug)]
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+impl FromStr for Color {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.strip_prefix("&H").unwrap_or(s);
+        Ok(match s.len() {
+            8 => Color {
+                r: u8::from_str_radix(&s[0..2], 16).unwrap(),
+                g: u8::from_str_radix(&s[2..4], 16).unwrap(),
+                b: u8::from_str_radix(&s[4..6], 16).unwrap(),
+                a: u8::from_str_radix(&s[6..8], 16).unwrap(),
+            },
+            oth => todo!("{}", oth),
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Style {
+    name: String,
+    font_name: String,
+    font_size: f32,
+    primary_colour: Color,
+    secondary_colour: Color,
+    outline_colour: Color,
+    back_colour: Color,
+    bold: bool,
+    italic: bool,
+    underline: bool,
+    strike_out: bool,
+    scale_x: f32,
+    scale_y: f32,
+    spacing: f32,
+    angle: f32,
+    border_style: (),
+    outline: f32,
+    shadow: f32,
+    alignment: Alignment,
+    margin_l: f32,
+    margin_r: f32,
+    margin_v: f32,
+    encoding: (),
+}
+
+
+fn parse_styles_mapping(s: &str) -> Option<StyleMapping> {
+    let mut map = StyleMapping {
+        name: usize::MAX,
+        font_name: usize::MAX,
+        font_size: usize::MAX,
+        primary_colour: usize::MAX,
+        secondary_colour: usize::MAX,
+        outline_colour: usize::MAX,
+        back_colour: usize::MAX,
+        bold: usize::MAX,
+        italic: usize::MAX,
+        underline: usize::MAX,
+        strike_out: usize::MAX,
+        scale_x: usize::MAX,
+        scale_y: usize::MAX,
+        spacing: usize::MAX,
+        angle: usize::MAX,
+        border_style: usize::MAX,
+        outline: usize::MAX,
+        shadow: usize::MAX,
+        alignment: usize::MAX,
+        margin_l: usize::MAX,
+        margin_r: usize::MAX,
+        margin_v: usize::MAX,
+        encoding: usize::MAX,
+    };
+    for (idx, s) in s.split(", ").enumerate() {
+        match s {
+            "Name" => map.name = idx,
+            "Fontname" => map.font_name = idx,
+            "Fontsize" => map.font_size = idx,
+            "PrimaryColour" => map.primary_colour = idx,
+            "SecondaryColour" => map.secondary_colour = idx,
+            "OutlineColour" => map.outline_colour = idx,
+            "BackColour" => map.back_colour = idx,
+            "Bold" => map.bold = idx,
+            "Italic" => map.italic = idx,
+            "Underline" => map.underline = idx,
+            "StrikeOut" => map.strike_out = idx,
+            "ScaleX" => map.scale_x = idx,
+            "ScaleY" => map.scale_y = idx,
+            "Spacing" => map.spacing = idx,
+            "Angle" => map.angle = idx,
+            "BorderStyle" => map.border_style = idx,
+            "Outline" => map.outline = idx,
+            "Shadow" => map.shadow = idx,
+            "Alignment" => map.alignment = idx,
+            "MarginL" => map.margin_l = idx,
+            "MarginR" => map.margin_r = idx,
+            "MarginV" => map.margin_v = idx,
+            "Encoding" => map.encoding = idx,
+            _ => unimplemented!(),
+        }
+    }
+
+    Some(map)
+}
+
 fn parse_file(path: impl AsRef<Path>) {
     let mut reader = BufReader::new(fs::File::open(path).unwrap());
-    let mut mapping = None;
+    let mut events_mapping: Option<EventMapping> = None;
+    let mut styles_mapping = None;
     let mut current_section = None;
     let mut buffer = String::new();
+    let mut styles = Vec::new();
+
+    let parse_event = |data: &str, mapping: &EventMapping, what: EventType| {
+        let fields = data.splitn(10, ',').collect::<Vec<_>>();
+        let line = Event {
+            marked: fields.get(mapping.marked) == Some(&"1"),
+            layer: fields[mapping.layer].parse().unwrap(),
+            start: fields[mapping.start].parse().unwrap(),
+            end: fields[mapping.end].parse().unwrap(),
+            style: fields[mapping.style],
+            name: fields[mapping.name],
+            margin_l: fields[mapping.margin_l].parse().ok(),
+            margin_r: fields[mapping.margin_r].parse().ok(),
+            margin_v: fields[mapping.margin_v].parse().ok(),
+            effect: fields[mapping.effect],
+            text: fields[mapping.text],
+        };
+
+        match parse(line.text.as_bytes()) {
+            Ok(res) => {
+                // println!("{:?}", res);
+            }
+            Err(r) => todo!("{:?}", r),
+        }
+    };
+
+    enum EventType {
+        Dialogue,
+        Comment,
+    }
 
     loop {
         buffer.clear();
@@ -159,38 +349,53 @@ fn parse_file(path: impl AsRef<Path>) {
             current_section = match section.strip_suffix(']').unwrap() {
                 "Events" => Some(Section::Events),
                 "Script Info" => Some(Section::ScriptInto),
+                "V4+ Styles" => Some(Section::V4Styles),
                 _ => None,
             };
         } else if let Some(format) = line.strip_prefix("Format: ") {
-            if current_section != Some(Section::Events) {
+            let Some(section) = current_section.as_ref() else {
                 continue;
-            }
-            mapping = make_mapping(format);
-        } else if let Some(x) = line.strip_prefix("Dialogue: ") {
-            let mapping = mapping.as_ref().unwrap();
-            let fields = x.splitn(10, ',').collect::<Vec<_>>();
-            let line = Event {
-                marked: fields.get(mapping.marked) == Some(&"1"),
-                layer: fields[mapping.layer].parse().unwrap(),
-                start: fields[mapping.start].parse().unwrap(),
-                end: fields[mapping.end].parse().unwrap(),
-                style: fields[mapping.style],
-                name: fields[mapping.name],
-                margin_l: fields[mapping.margin_l].parse().ok(),
-                margin_r: fields[mapping.margin_r].parse().ok(),
-                margin_v: fields[mapping.margin_v].parse().ok(),
-                effect: fields[mapping.effect],
-                text: fields[mapping.text],
             };
 
-            match parse(line.text.as_bytes()) {
-                Ok(res) => {
-                    // println!("{:?}", res);
-                }
-                Err(r) => todo!("{:?}", r),
+            match section {
+                Section::Events => events_mapping = parse_events_mapping(format),
+                Section::V4Styles => styles_mapping = parse_styles_mapping(format),
+                _ => todo!(),
             }
+        } else if let Some(x) = line.strip_prefix("Dialogue: ") {
+            parse_event(x, events_mapping.as_ref().unwrap(), EventType::Dialogue);
         } else if let Some(x) = line.strip_prefix("Comment: ") {
-            // TODO
+            parse_event(x, events_mapping.as_ref().unwrap(), EventType::Comment);
+        } else if let Some(s) = line.strip_prefix("Style: ") {
+            let mapping = styles_mapping.as_ref().unwrap();
+            let fields = s.split(',').collect::<Vec<_>>();
+            dbg!(&fields);
+            let style = Style {
+                name: fields[mapping.name].to_string(),
+                font_name: fields[mapping.font_name].parse().unwrap(),
+                font_size: fields[mapping.font_size].parse().unwrap(),
+                primary_colour: fields[mapping.primary_colour].parse().unwrap(),
+                secondary_colour: fields[mapping.secondary_colour].parse().unwrap(),
+                outline_colour: fields[mapping.outline_colour].parse().unwrap(),
+                back_colour: fields[mapping.back_colour].parse().unwrap(),
+                bold: fields[mapping.bold] == "1",
+                italic: fields[mapping.italic] == "1",
+                underline: fields[mapping.underline] == "1",
+                strike_out: fields[mapping.strike_out] == "1",
+                scale_x: fields[mapping.scale_x].parse().unwrap(),
+                scale_y: fields[mapping.scale_y].parse().unwrap(),
+                spacing: fields[mapping.spacing].parse().unwrap(),
+                angle: fields[mapping.angle].parse().unwrap(),
+                border_style: (),
+                outline: fields[mapping.outline].parse().unwrap(),
+                shadow: fields[mapping.shadow].parse().unwrap(),
+                alignment: Alignment(fields[mapping.alignment].parse().unwrap()),
+                margin_l: fields[mapping.margin_l].parse().unwrap(),
+                margin_r: fields[mapping.margin_r].parse().unwrap(),
+                margin_v: fields[mapping.margin_v].parse().unwrap(),
+                encoding: (),
+            };
+            styles.push(dbg!(style));
         } else if let Some((name, value)) = line.split_once(": ") {
             println!("{} = {}", name, value);
         } else if line.starts_with(";") {
